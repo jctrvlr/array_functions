e20c9a58fff1ca89cfaba11f06a15edcb276d480
jctrvlr <jcummings5@gmail.com>
2016-10-13 11:21:52 -0400

    Added link to index to README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # array_functions
+To see examples in action go to: https://web.njit.edu/~jic6/array_functions/ 
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP function examples</title>
+</head>
+<body>
+<h1> PHP function examples </h1>
+<ol>
+  <li><a href="array_change_key_case.php">array_change_key_case()</a></li>
+  <li><a href="array_chunk.php">array_chunk()</a></li>
+  <li><a href="array_combine.php">array_combine()</a></li>
+  <li><a href="array_count_values.php">array_count_values()</a></li>
+  <li><a href="array_diff.php">array_diff()</a></li>
+  <li><a href="array_fill.php">array_fill()</a></li>
+  <li><a href="array_filter.php">array_filter()</a></li>
+  <li><a href="array_keys.php">array_keys()</a></li>
+  <li><a href="array_pad.php">array_pad()</a></li>
+  <li><a href="array_pop.php">array_pop()</a></li>
+  <li><a href="array_push.php">array_push()</a></li>
+  <li><a href="array_reverse.php">array_reverse()</a></li>
+  <li><a href="array_search.php">array_search()</a></li>
+  <li><a href="array_udiff.php">array_udiff()</a></li>
+  <li><a href="array_unique.php">array_unique()</a></li>
+  <li><a href="array_walk.php">array_walk()</a></li>
+</ol>
+</body>
+</html>
